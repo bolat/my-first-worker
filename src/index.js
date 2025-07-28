9147fa2c2abf6c536611717dbb6bc2c10be9ee86
Boulat Ashim <bolat.ashimov@gmail.com>
2025-07-28 23:19:35 +0500

    const auth = new google.auth.GoogleAuth({ 			credentials: JSON.parse(APIkey), 			scopes: ['https://www.googleapis.com/auth/calendar.readonly'],

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { env } from 'cloudflare:workers';
+import { google } from 'googleapis';
 
 // src/index.js
 // This is a Cloudflare Worker that uses an environment variable for a Google Service Account key.
@@ -6,6 +7,14 @@ export default {
 	async fetch(request, env, ctx) {
 		const APIkey = await env.GOOGLE_SERVICE_ACCOUNT_KEY.get();
 		// Example of using the secret safely in an API request
+		const auth = new google.auth.GoogleAuth({
+			credentials: JSON.parse(APIkey),
+			scopes: ['https://www.googleapis.com/auth/calendar.readonly'],
+		});
+		// const auth = new google.auth.GoogleAuth({
+		// 	credentials: JSON.parse(APIkey),
+		// 	scopes: ['https://www.googleapis.com/auth/calendar.readonly'],
+		// });
 		return new Response('Hello Worker! '+ APIkey);
 	},
 };
